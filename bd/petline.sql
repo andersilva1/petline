751c2eafc922f7a0563cca86bb576e4c18e4d656
Anderson Rodrigues <andersonrodrigues209@gmail.com>
2018-11-08 16:36:56 -0200

    Inclusao de novo banco

--- bd/petline.sql
+++ bd/petline.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2018-11-04 22:01:44
+Date: 2018-11-08 16:36:19
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -88,38 +88,40 @@ CREATE TABLE `pacote` (
   `id_usuario` int(11) DEFAULT NULL,
   `dt_alteracao` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `ativo` char(1) DEFAULT NULL,
+  `pet_pego` char(1) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `idx_pacote` (`id`),
   KEY `fk_id_usuario_2` (`id_usuario`),
   CONSTRAINT `fk_id_usuario_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of pacote
 -- ----------------------------
-INSERT INTO `pacote` VALUES ('2', 'LINE_BASIC', '1', '2018-10-20', '07:18:00', '08:18:00', '2', '2018-11-02 16:51:17', '0');
-INSERT INTO `pacote` VALUES ('3', 'LINE_BASIC', '1', '2018-10-21', '05:05:00', '05:05:00', '2', '2018-10-25 13:21:16', '1');
-INSERT INTO `pacote` VALUES ('4', 'LINE_BASIC', '1', '2018-10-23', '10:00:00', '11:00:00', '2', '2018-10-22 21:04:25', '1');
-INSERT INTO `pacote` VALUES ('5', 'LINE_BASIC', '1', '2018-10-24', '10:00:00', '11:00:00', '2', '2018-10-25 13:13:56', '1');
-INSERT INTO `pacote` VALUES ('6', 'LINE_BASIC', '1', '2018-10-25', '10:00:00', '11:00:00', '2', '2018-10-25 13:14:14', '1');
-INSERT INTO `pacote` VALUES ('7', 'LINE_BASIC', '1', '2018-10-26', '10:00:00', '11:00:00', '2', '2018-10-25 13:14:32', '1');
-INSERT INTO `pacote` VALUES ('8', 'LINE_BASIC', '1', '2018-10-27', '10:00:00', '11:00:00', '2', '2018-10-25 13:14:45', '1');
-INSERT INTO `pacote` VALUES ('9', 'LINE_BASIC', '1', '2018-10-28', '11:00:00', '12:00:00', '2', '2018-10-25 13:14:56', '1');
-INSERT INTO `pacote` VALUES ('12', 'LINE_BASIC', '1', '2018-10-12', '15:09:00', '16:09:00', '8', '2018-10-26 12:42:24', '1');
-INSERT INTO `pacote` VALUES ('13', 'LINE_BASIC', '1', '2018-10-12', '18:00:00', '20:00:00', '8', '2018-10-26 12:42:47', '1');
-INSERT INTO `pacote` VALUES ('14', 'LINE_BASIC', '1', '2018-11-05', '05:05:00', '05:05:00', '8', '2018-10-26 13:08:25', '1');
-INSERT INTO `pacote` VALUES ('15', 'RICH_DOG', '1', '2018-10-20', '10:10:00', '11:10:00', '7', '2018-11-02 16:52:16', '0');
-INSERT INTO `pacote` VALUES ('16', 'RICH_DOG', '1', '2018-10-21', '10:00:00', '11:00:00', '7', '2018-11-02 13:08:01', '1');
-INSERT INTO `pacote` VALUES ('17', 'RICH_DOG', '1', '2018-10-22', '12:00:00', '13:00:00', '7', '2018-11-02 13:08:18', '1');
-INSERT INTO `pacote` VALUES ('18', 'RICH_DOG', '1', '2018-10-23', '14:00:00', '15:00:00', '7', '2018-11-02 13:08:42', '1');
-INSERT INTO `pacote` VALUES ('19', 'RICH_DOG', '1', '2018-10-24', '15:00:00', '16:00:00', '7', '2018-11-02 13:08:54', '1');
-INSERT INTO `pacote` VALUES ('20', 'RICH_DOG', '1', '2018-10-25', '12:00:00', '13:00:00', '7', '2018-11-02 13:09:03', '1');
-INSERT INTO `pacote` VALUES ('21', 'RICH_DOG', '1', '2018-10-26', '13:00:00', '14:00:00', '7', '2018-11-02 13:09:17', '1');
-INSERT INTO `pacote` VALUES ('22', 'RICH_DOG', '1', '2018-10-27', '13:10:00', '13:50:00', '7', '2018-11-02 13:09:43', '1');
-INSERT INTO `pacote` VALUES ('23', 'RICH_DOG', '1', '2018-10-28', '05:10:00', '06:00:00', '7', '2018-11-02 13:09:53', '1');
-INSERT INTO `pacote` VALUES ('24', 'RICH_DOG', '1', '2018-10-29', '07:00:00', '08:00:00', '7', '2018-11-02 13:10:10', '1');
-INSERT INTO `pacote` VALUES ('25', 'RICH_DOG', '1', '2018-10-30', '13:00:00', '14:00:00', '7', '2018-11-02 13:10:20', '1');
-INSERT INTO `pacote` VALUES ('26', 'RICH_DOG', '1', '2018-11-01', '10:00:00', '10:30:00', '7', '2018-11-02 13:10:39', '1');
+INSERT INTO `pacote` VALUES ('2', 'LINE_BASIC', '1', '2018-10-20', '07:18:00', '08:18:00', '2', '2018-11-07 12:59:28', '0', '0');
+INSERT INTO `pacote` VALUES ('3', 'LINE_BASIC', '1', '2018-10-21', '05:05:00', '05:05:00', '2', '2018-11-07 13:00:59', '0', '0');
+INSERT INTO `pacote` VALUES ('4', 'LINE_BASIC', '1', '2018-10-23', '10:00:00', '11:00:00', '2', '2018-11-07 13:04:19', '1', '1');
+INSERT INTO `pacote` VALUES ('5', 'LINE_BASIC', '1', '2018-10-24', '10:00:00', '11:00:00', '2', '2018-11-07 13:34:22', '1', '1');
+INSERT INTO `pacote` VALUES ('6', 'LINE_BASIC', '1', '2018-10-25', '10:00:00', '11:00:00', '2', '2018-11-07 12:59:28', '1', '0');
+INSERT INTO `pacote` VALUES ('7', 'LINE_BASIC', '1', '2018-10-26', '10:00:00', '11:00:00', '2', '2018-11-07 12:59:28', '1', '0');
+INSERT INTO `pacote` VALUES ('8', 'LINE_BASIC', '1', '2018-10-27', '10:00:00', '11:00:00', '2', '2018-11-07 12:59:28', '1', '0');
+INSERT INTO `pacote` VALUES ('9', 'LINE_BASIC', '1', '2018-10-28', '11:00:00', '12:00:00', '2', '2018-11-07 12:59:28', '1', '0');
+INSERT INTO `pacote` VALUES ('12', 'LINE_BASIC', '1', '2018-10-12', '15:09:00', '16:09:00', '8', '2018-11-07 12:59:28', '1', '0');
+INSERT INTO `pacote` VALUES ('13', 'LINE_BASIC', '1', '2018-10-12', '18:00:00', '20:00:00', '8', '2018-11-07 12:59:28', '1', '0');
+INSERT INTO `pacote` VALUES ('14', 'LINE_BASIC', '1', '2018-11-05', '05:05:00', '05:05:00', '8', '2018-11-07 12:59:28', '1', '0');
+INSERT INTO `pacote` VALUES ('15', 'RICH_DOG', '1', '2018-10-20', '10:10:00', '11:10:00', '7', '2018-11-07 12:59:28', '0', '0');
+INSERT INTO `pacote` VALUES ('16', 'RICH_DOG', '1', '2018-10-21', '10:00:00', '11:00:00', '7', '2018-11-07 12:59:28', '1', '0');
+INSERT INTO `pacote` VALUES ('17', 'RICH_DOG', '1', '2018-10-22', '12:00:00', '13:00:00', '7', '2018-11-07 12:59:28', '1', '0');
+INSERT INTO `pacote` VALUES ('18', 'RICH_DOG', '1', '2018-10-23', '14:00:00', '15:00:00', '7', '2018-11-07 12:59:28', '1', '0');
+INSERT INTO `pacote` VALUES ('19', 'RICH_DOG', '1', '2018-10-24', '15:00:00', '16:00:00', '7', '2018-11-07 12:59:28', '1', '0');
+INSERT INTO `pacote` VALUES ('20', 'RICH_DOG', '1', '2018-10-25', '12:00:00', '13:00:00', '7', '2018-11-07 12:59:28', '1', '0');
+INSERT INTO `pacote` VALUES ('21', 'RICH_DOG', '1', '2018-10-26', '13:00:00', '14:00:00', '7', '2018-11-07 12:59:28', '1', '0');
+INSERT INTO `pacote` VALUES ('22', 'RICH_DOG', '1', '2018-10-27', '13:10:00', '13:50:00', '7', '2018-11-07 12:59:28', '1', '0');
+INSERT INTO `pacote` VALUES ('23', 'RICH_DOG', '1', '2018-10-28', '05:10:00', '06:00:00', '7', '2018-11-07 12:59:28', '1', '0');
+INSERT INTO `pacote` VALUES ('24', 'RICH_DOG', '1', '2018-10-29', '07:00:00', '08:00:00', '7', '2018-11-07 12:59:28', '1', '0');
+INSERT INTO `pacote` VALUES ('25', 'RICH_DOG', '1', '2018-10-30', '13:00:00', '14:00:00', '7', '2018-11-07 12:59:28', '1', '0');
+INSERT INTO `pacote` VALUES ('26', 'RICH_DOG', '1', '2018-11-01', '10:00:00', '10:30:00', '7', '2018-11-07 12:59:28', '1', '0');
+INSERT INTO `pacote` VALUES ('27', 'LINE_BASIC', '1', '2018-11-06', '09:00:00', '10:00:00', '2', '2018-11-07 12:59:28', '1', '0');
 
 -- ----------------------------
 -- Table structure for `passeador`
@@ -235,15 +237,15 @@ CREATE TABLE `servico` (
 -- ----------------------------
 -- Records of servico
 -- ----------------------------
-INSERT INTO `servico` VALUES ('1', '3', '2', '4', null, '2018-11-02 16:38:17', '2', '0', '0');
-INSERT INTO `servico` VALUES ('2', '3', '2', '5', null, '2018-11-02 16:00:24', '2', '0', '0');
-INSERT INTO `servico` VALUES ('3', '3', '2', '4', null, '2018-11-01 21:14:09', '2', '1', '0');
-INSERT INTO `servico` VALUES ('4', '3', '2', '4', null, '2018-11-01 23:14:50', '2', '1', '0');
-INSERT INTO `servico` VALUES ('5', '3', '2', '4', null, '2018-11-01 23:38:19', '2', '1', '0');
-INSERT INTO `servico` VALUES ('6', '3', '2', '10', null, '2018-11-02 16:00:27', '2', '0', '0');
-INSERT INTO `servico` VALUES ('7', '3', '15', '11', null, '2018-11-02 16:37:33', '7', '0', '0');
-INSERT INTO `servico` VALUES ('8', '3', '15', '11', null, '2018-11-02 16:42:51', '7', '0', '0');
-INSERT INTO `servico` VALUES ('11', '3', '15', '11', null, '2018-11-02 16:42:56', '7', '0', '0');
+INSERT INTO `servico` VALUES ('1', '3', '2', '4', null, '2018-11-07 12:54:14', '2', '0', '0');
+INSERT INTO `servico` VALUES ('2', '3', '2', '5', null, '2018-11-07 12:54:14', '2', '0', '0');
+INSERT INTO `servico` VALUES ('3', '3', '2', '4', null, '2018-11-07 12:54:14', '2', '1', '0');
+INSERT INTO `servico` VALUES ('4', '3', '2', '4', null, '2018-11-07 12:54:14', '2', '1', '0');
+INSERT INTO `servico` VALUES ('5', '3', '2', '4', null, '2018-11-07 12:54:14', '2', '1', '0');
+INSERT INTO `servico` VALUES ('6', '3', '2', '10', null, '2018-11-07 12:54:14', '2', '0', '0');
+INSERT INTO `servico` VALUES ('7', '3', '15', '11', null, '2018-11-07 12:54:14', '7', '0', '0');
+INSERT INTO `servico` VALUES ('8', '3', '15', '11', null, '2018-11-07 12:54:14', '7', '0', '0');
+INSERT INTO `servico` VALUES ('11', '3', '15', '11', null, '2018-11-07 12:54:14', '7', '0', '0');
 
 -- ----------------------------
 -- Table structure for `usuario`
@@ -281,10 +283,10 @@ CREATE TABLE `usuario` (
 INSERT INTO `usuario` VALUES ('1', 'Administrador', 'Petline', 'admin', '23d42f5f3f66498b2c8ff4c20b8c5ac826e47146', 'administrador@petline.com', '(00) 00000-0', '00000000', '000000000', 'DF', '72745-011', 'Brasília', 'Quadra 45 Conjunto K', 'Vila São José (Brazlândia)', '2018-10-09', '', 'adm', '1', '2018-09-13 20:31:02');
 INSERT INTO `usuario` VALUES ('2', 'Cliente', 'Petline', 'cliente', 'f7c3bc1d808e04732adf679965ccc34ca7ae3441', 'cliente@petline.com', '(61) 99356-4879', '19.878.131-6', '635.460.591-23', 'MG', '30510-410', 'Belo Horizonte', 'Rua Azaléia', 'Nova Gameleira', '2018-10-16', '', 'cli', '1', '2018-10-18 11:30:04');
 INSERT INTO `usuario` VALUES ('3', 'Passeador', 'Petline', 'passeador', 'f7c3bc1d808e04732adf679965ccc34ca7ae3441', 'passeador@passeador.com', '(61) 93265-9898', '16.589.876', '171.418.723-37', 'RJ', '23575-385', 'Rio de Janeiro', 'Travessa Dom Diniz', 'Santa Cruz', '2018-10-09', '', 'pas', '1', '2018-10-18 11:29:24');
-INSERT INTO `usuario` VALUES ('4', 'Rafael', 'Souza', 'rafael', 'cbf2510a5f9f7eece23428da7125c06115839e2b', 'rafael@gmail.com', '(65) 49846-5168', '21.498.465-1', '726.491.981-17', 'GO', '72855-239', 'Luziânia', 'Quadra 239', 'Parque Industrial Mingone', '2018-10-04', '', 'cli', '1', '2018-11-01 23:51:38');
+INSERT INTO `usuario` VALUES ('4', 'Rafael', 'Souza', 'rafael', 'f7c3bc1d808e04732adf679965ccc34ca7ae3441', 'rafael@gmail.com', '(65) 49846-5168', '21.498.465-1', '726.491.981-17', 'GO', '72855-239', 'Luziânia', 'Quadra 239', 'Parque Industrial Mingone', '2018-10-04', '', 'cli', '1', '2018-11-05 14:32:51');
 INSERT INTO `usuario` VALUES ('5', 'Rafael', 'Rodrigues', 'rafael1', '1f49d78abdcbe13fcf991ec4a4ee60fcdf8b7f4b', 'rafael2@gmail.com', '(44) 44444-4444', '65.198.465-1', '426.187.374-58', 'DF', '72738-000', 'Brasília', 'Quadra 38', 'Vila São José (Brazlândia)', '2018-10-11', 'Onde quer que você esteja, você sempre estará lá!!!!!', 'pas', '1', '2018-09-20 13:59:05');
 INSERT INTO `usuario` VALUES ('6', 'Evaldo', 'Junior', 'evaldo', '3e2fc5e632b74c065a9a5efa054acacd7b14475c', 'evaldo@gmail.com', '(61) 32132-1321', '11.111.284-8', '000.000.000-00', 'DF', '72745-011', 'Brasília', 'Quadra 45 Conjunto K', 'Vila São José (Brazlândia)', '2018-10-09', 'Sou passeador e gosto de passear com cães', 'pas', '1', '2018-09-20 19:25:32');
-INSERT INTO `usuario` VALUES ('7', 'teste', 'teste', 'anderson1', 'f7c3bc1d808e04732adf679965ccc34ca7ae3441', 'email@email.com', '(65) 64684-8684', '16.546.848-6', '057.799.111-69', 'DF', '72745-011', 'Brasília', 'Quadra 45 Conjunto K', 'Vila São José (Brazlândia)', '2011-10-10', '', 'cli', '1', '2018-11-02 13:05:41');
+INSERT INTO `usuario` VALUES ('7', 'Anderson', 'Rodrigues', 'anderson1', 'f7c3bc1d808e04732adf679965ccc34ca7ae3441', 'andersonrodrigues209@gmail.com', '(61) 99332-4195', '16.546.848-6', '057.799.111-69', 'DF', '72745-011', 'Brasília', 'Quadra 45 Conjunto K', 'Vila São José (Brazlândia)', '1996-11-17', '', 'cli', '1', '2018-11-06 12:52:08');
 INSERT INTO `usuario` VALUES ('8', 'Gislane', 'Santana', 'gislane', 'f7c3bc1d808e04732adf679965ccc34ca7ae3441', 'santana1204@gmail.com', '(61) 99999-9999', '11.111.111-1', '830.002.402-68', '..', '72745-011', '...', '...', '...', '0000-00-00', '', 'cli', '1', '2018-10-25 19:30:51');
 INSERT INTO `usuario` VALUES ('9', 'Sarah', 'Guedes', 'sarahlinda', 'e94ddd1dc4bee199d0a5452ef42ff4205dbbc9ea', 'sarah@gmail.com', '(61) 95651-6849', '2151551', '629.353.633-96', 'DF', '72738-000', 'Brasília', 'Quadra 38', 'Vila São José (Brazlândia)', '2018-01-01', 'A Sarah é MUITO feia!', 'pas', '1', '2018-11-01 22:26:51');
 
